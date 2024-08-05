719ec2304e246ba3b36e02d68d86a7c3df68a31c
Hailey-Z <hailey.zhang1@monash.edu>
2024-08-05 16:34:55 +1000

    added package to global file

--- global.R
+++ global.R
@@ -1,10 +1,13 @@
+VERSION <- "v0.1"
+
 library("shiny")
 library("shinyjs")
+library("shiny.info")
 library("bslib")
 library("tidyverse")
 library("DT")
-library("DEP")
 library("ggplot2")
 library("boot")
 
 source("R/functions.R")
+source("R/gg_theme.R")
